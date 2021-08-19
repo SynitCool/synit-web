555b1733ba2dbedbd115f15770dfa747cf07d1a6
BASI <zolarpixel3@gmail.com>
2021-08-19 20:51:13 +0700

    Add project

---
+++ app.py
@@ -0,0 +1,24 @@
+import flask
+from flask import Flask, jsonify, request, render_template, url_for
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def base():
+    return render_template("index.html")
+
+@app.route('/project')
+def project():
+    return render_template("project.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
