260472ccb37c075a7e48ff7430c1bb67b65fd89f
SynitBackUp <synit.1st@gmail.com>
2021-10-31 21:24:57 +0700

    config route keyar_mining_api

--- synitweb/keyar_mining_api.py
+++ synitweb/keyar_mining_api.py
@@ -2,6 +2,30 @@ from flask import Blueprint, render_template
 
 keyar_mining_api = Blueprint('keyar_mining_api', __name__)
 
+# Overview
 @keyar_mining_api.route("/keyar-mining/api")
 def api_overview():
     return render_template("keyar_mining/api_pages/api_overview.html")
+
+# Association rules
+@keyar_mining_api.route("/keyar-mining/api/association-rules/apriori")
+def apriori():
+    return render_template("keyar_mining/api_pages/association_rules/apriori.html")
+
+@keyar_mining_api.route("/keyar-mining/api/association-rules/fpgrowth")
+def fpgrowth():
+    return render_template("keyar_mining/api_pages/association_rules/fpgrowth.html")
+
+@keyar_mining_api.route("/keyar-mining/api/association-rules/evaluate")
+def evaluate():
+    return render_template("keyar_mining/api_pages/association_rules/evaluate.html")
+
+# Clustering
+@keyar_mining_api.route("/keyar-mining/api/clustering/kmeans")
+def kmeans():
+    return render_template("keyar_mining/api_pages/clustering/kmeans.html")
+
+# Classification
+@keyar_mining_api.route("/keyar-mining/api/classification/knn")
+def knn():
+    return render_template("keyar_mining/api_pages/classification/knn.html")
